a02041e28757be944cf6301b5c15358cde442e62
Jaime Crespo <jynus@users.noreply.github.com>
2026-05-24 21:36:58 +0200

    +Export Presets

--- scripts/game.gd
+++ scripts/game.gd
@@ -50,19 +50,6 @@ func _ready() -> void:
 	#	print_debug(m.pos)
 	dead = false
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_just_pressed("fullscreen"):
-		toggle_fullscreen()
-
-func toggle_fullscreen() -> void:
-	if DisplayServer.window_get_mode() in [
-		DisplayServer.WINDOW_MODE_FULLSCREEN,
-		DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN
-	]:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-
 func restart_game():
 	get_tree().reload_current_scene()
 
